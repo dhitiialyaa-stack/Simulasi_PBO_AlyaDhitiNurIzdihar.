7f4ec7b504965a3ee4bca7ac71a2408b88a4467b
alya <dhitiialyaa@gmail.com>
2026-06-17 10:11:26 +0700

    [Tahap 3] Membuat file koneksi database dan abstract class Pendaftaran beserta properti & method abstraknya

---
+++ koneksi.php
@@ -0,0 +1,28 @@
+<?php
+// koneksi.php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "DB_SIMULASI_PBO_TI1C_AlyaDhitiNurIzdihar"; 
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            // Mengatur error mode PDO ke Exception untuk memudahkan debugging
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Koneksi database gagal: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
